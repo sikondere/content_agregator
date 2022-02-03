e01870b431887acb907feb027e9e20ba30a87484
sikondere <nimrod85@yahoo.com>
2022-02-03 12:43:16 +0200

    created home page view

--- podcasts/tests.py
+++ podcasts/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.utils import timezone
+from django.urls.base import reverse
+
 from .models import Episode
 
 # Create your tests here.
@@ -22,3 +24,15 @@ class PodcastsTests(TestCase):
 
     def test_episode_str_representation(self):
         self.assertEqual(str(self.episode), "My Python Podcast: My Awesome Podcast Episode")
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_home_page_uses_correct_template(self):
+        response = self.client.get(reverse('homepage'))
+        self.assertTemplateUsed(response, 'homepage.html')
+
+    def test_home_page_list_contents(self):
+        response = self.client.get(reverse('homepage'))
+        self.assertContains(response, 'My Awesome Podcast Episode')
--- podcasts/views.py
+++ podcasts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from typing import Any, Dict
+
+from .models import Episode
 
 # Create your views here.
+class HomePageView(ListView):
+    template_name = 'homepage.html'
+    model = Episode
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['episodes'] = Episode.objects.filter().order_by('-pub_date')[:10]
+        return context
